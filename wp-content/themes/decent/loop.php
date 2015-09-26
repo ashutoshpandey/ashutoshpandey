5ab590a058236908c1fdf6f71e9fa2a3485ccad8
ashutoshpandey <ashutoshpandey.in@gmail.com>
2015-09-26 11:22:02 +0530

    style.css and loop.php update

--- wp-content/themes/decent/loop.php
+++ wp-content/themes/decent/loop.php
@@ -22,12 +22,15 @@
 
                   <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo __('Permalink to', 'decent'). ' ' ?><?php the_title_attribute() ?>"><?php the_title() ?></a></h1>
 
-                <div class="loop-post-meta">
-                     <span><?php _e('Written on', 'decent') ?> </span><span class="loop-meta-date"><?php echo get_the_time('M, d, Y') ?></span>
-                     <span><?php _e('by', 'decent') ?> </span><span class="loop-meta-author"><?php the_author_posts_link() ?></span>
-                     <span class="loop-meta-comments"> | <?php comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'No Comments'); ?></span>
-                </div>
+<!--                <div class="loop-post-meta">-->
+<!--                     <span>--><?php //_e('Written on', 'decent') ?><!-- </span><span class="loop-meta-date">--><?php //echo get_the_time('M, d, Y') ?><!--</span>-->
+<!--                     <span>--><?php //_e('by', 'decent') ?><!-- </span><span class="loop-meta-author">--><?php //the_author_posts_link() ?><!--</span>-->
+<!--                     <span class="loop-meta-comments"> | --><?php //comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'No Comments'); ?><!--</span>-->
+<!--                </div>-->
 
+                 <div class="loop-post-meta">
+                     <span><?php _e('Written on', 'decent') ?> </span><span class="loop-meta-date"><?php echo get_the_time('M, d, Y') ?></span>
+                 </div>
              </div>
 
              <div class="loop-post-excerpt clearfix">
